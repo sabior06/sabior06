56108344a4ce6b0f1557181745ec38170ebf6d60
sabior06 <sabior06@gmail.com>
2019-12-11 18:27:58 +0200

    job done

---
+++ lesson 17/17 home task/script.js
@@ -0,0 +1,54 @@
+"use strict"
+
+ let seconds_In_Third_Hours;
+
+let our_Hours = 3;
+
+let minuts_In_One_Hour = 60;
+
+let seconds_In_One_Minut = 60;
+
+    seconds_In_Third_Hours = our_Hours * minuts_In_One_Hour * seconds_In_One_Minut;
+
+    console.log(seconds_In_Third_Hours);
+
+let num = 1;
+
+num += 12;
+
+num -= 14;
+
+num *=  5;
+
+num /= 7;
+
+num ++;
+
+num --;
+
+alert(num);
+
+
+
+
+
+let a = 10, b = 2, c = 5;
+
+console.log(a + b + c);
+
+
+ a = 14;
+
+b = 10;
+
+c = a - b;
+
+let d = 7;
+
+let result = c + d;
+
+console.log(result);
+
+let name = 'sabior';
+
+console.log("hello, " + "%" + name + "%!");
