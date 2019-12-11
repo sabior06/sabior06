f73c5beefec65bf24dc098cb6980ec7ff96beaf5
sabior06 <sabior06@gmail.com>
2019-12-11 18:41:47 +0200

    task 18

---
+++ lesson 18/18 home task/script.js
@@ -0,0 +1,99 @@
+'use strict'
+
+ let a = prompt('Insert the number', '');
+
+console.log(a);
+
+if(a === '1'){
+
+    console.log('Right');
+
+   }else{
+
+       console.log('Not true');
+
+   }
+
+
+
+ 
+  let item = false;
+
+if(item == false){
+
+    console.log("Right");
+
+}else{
+
+    console.log("Not true");
+
+}
+
+ 
+
+let value = false; 
+
+let item = (value == false) ? console.log("Right") : console.log("Not true");
+
+
+
+
+
+
+
+ let a = -3;
+
+if(a >= 1 && a <= 4){
+
+    console.log("Right");
+
+}else{
+
+    console.log("Not true");
+
+} 
+
+
+
+  let a = prompt('enter the number a', '');
+
+let b = prompt('enter the number b', '');
+
+if((a >= 3 && a < 12) || (b >= 7 && b < 15)){
+
+    console.log("Right");
+
+}else{
+
+    console.log("Not true");
+
+}
+
+
+
+
+  let month = prompt('Enter a number from 1 to 12', '');
+
+ if(month == 1 || month <= 2 || month == 12){
+
+     console.log("Winter is now");
+
+ }else if(month == 3 || month <=5){
+
+     console.log("Spring is now");
+
+ }else if(month == 4 || month <=8){
+
+     console.log("It's summer now");
+
+ }else if(month == 9 || month <=11){
+
+     console.log("It's Autumn");
+
+ }else{
+
+     console.log('There is no such month in nature');
+
+ }
+
+    
